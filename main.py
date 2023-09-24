73499e1e17be6525443dfb7924594c4860cbcfa8
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-09-24 15:29:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import smtplib as s
 ob = s.SMTP("smtp.gmail.com", 587)
 ob.ehlo()
 ob.starttls()
-ob.login("biruktariku13@gmail.com", "BT1234BT")
+ob.login("example@gmail.com", "example pass")
 subject = "Hello"
 body = "testing the code"
 message = "subject:{}\n\n{}".format(subject, body)
-listadd = ["birukyetariku12@gmail.com", "biruktariku02@gmail.com"]
-ob.sendmail("biruktariku13@gmail.com", listadd, message)
+listadd = ["example_123@gmail.com", "example2202@gmail.com"]
+ob.sendmail("example@gmail.com", listadd, message)
 print("send mail")
 ob.quit()
